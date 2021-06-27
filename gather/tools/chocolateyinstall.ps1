46f6f3d452069cd806ba1f3e1dd9c9a10288984f
Michael T Lombardi <michael.t.lombardi@gmail.com>
2021-06-27 08:22:40 -0500

    (MAINT) Re-generalize package template
    Prior to this commit, the package template was committed with
    the version, download URL, and checksum filled out for v0.0.9
    instead of using the template values. This caused the script
    to always atempt to rerelease 0.0.9 instead of any newer
    version released.
    
    This commit returns the values to their template settings to
    enable build and automation.

--- gather/tools/chocolateyinstall.ps1
+++ gather/tools/chocolateyinstall.ps1
@@ -1,5 +1,5 @@
 $ErrorActionPreference = 'Stop';
-$toolsDir     = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$toolsDir = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
 
 $packageArgs = @{
   packageName    = $env:ChocolateyPackageName
@@ -8,8 +8,8 @@ $packageArgs = @{
   fileType       = 'exe'
   silentArgs     = '/S'
   validExitCodes = @(0)
-  url            = 'https://github.com/gathertown/gather-town-desktop-releases/releases/download/v0.0.9/Gather-Setup-0.0.9.exe'
-  checksum       = '498DFFC48FD3D53A1CC65C564C99627084DBC931B163FF203CD4FF1F482614EF'
+  url            = '[[URL]]'
+  checksum       = '[[CHECKSUM]]'
   checksumType   = 'sha256'
 }
 
